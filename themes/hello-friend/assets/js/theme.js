fa5393cc9ecdd33aa871c953a79854d4355901a6
Nate Gay <email@nategay.me>
2021-03-06 14:46:23 -0600

    Update auto dark mode script

--- themes/hello-friend/assets/js/theme.js
+++ themes/hello-friend/assets/js/theme.js
@@ -1,9 +1,21 @@
-// Use system dark mode
+// Toggle theme dark mode
+function enableDarkMode(enable) {
+  document.body.classList.toggle("dark-theme", enable);
+}
+
+// On page load, set the theme
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  enableDarkMode(true);
+} else {
+  enableDarkMode(false);
+}
+
+// On system change, update the theme
 window.matchMedia('(prefers-color-scheme: dark)')
       .addEventListener('change', event => {
   if (event.matches) {
-    document.body.classList.toggle("dark-theme", "dark");
+    enableDarkMode(true);
   } else {
-    document.body.classList.toggle("dark-theme", "light");
+    enableDarkMode(false);
   }
 })
